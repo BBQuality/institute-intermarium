3cf3619bce6c5fd4825dea95928929325a61eb6c
causesauce <adrian.chervinchuk1@gmail.com>
2023-11-07 21:40:54 -0500

    changed frontend for articles and news

--- src/route/api/articles.js
+++ src/route/api/articles.js
@@ -5,33 +5,33 @@ const mongoose = require('mongoose')
 const ArticleModel = require("../../models/Article")
 
 
-router.post('/postOne', async(req, res) => {
-    const reqObj = req.body;
+// router.post('/postOne', async(req, res) => {
+//     const reqObj = req.body;
 
-    const newData = new ArticleModel({
-        title: reqObj.title,
-        content : reqObj.content,
-        author: new mongoose.Types.ObjectId(reqObj.authorId),
-        summary: reqObj.summary
-    });
-    // console.log({title: reqObj.title,
-    //     content : reqObj.content,
-    //     author: reqObj.authorId,
-    //     summary: reqObj.summary});
-    // console.log(newData);
-    try{
-        const savedData = await newData.save();
-        res.status(201).json(savedData);
-    }
-    catch(error){
-        console.log(error);
-        res.status(400).json(error);
-    }
-});
+//     const newData = new ArticleModel({
+//         title: reqObj.title,
+//         content : reqObj.content,
+//         author: new mongoose.Types.ObjectId(reqObj.authorId),
+//         summary: reqObj.summary
+//     });
+//     // console.log({title: reqObj.title,
+//     //     content : reqObj.content,
+//     //     author: reqObj.authorId,
+//     //     summary: reqObj.summary});
+//     // console.log(newData);
+//     try{
+//         const savedData = await newData.save();
+//         res.status(201).json(savedData);
+//     }
+//     catch(error){
+//         console.log(error);
+//         res.status(400).json(error);
+//     }
+// });
 
 router.get('/getall', async(req, res) => {
     try{
-        const data = await ArticleModel.find();
+        const data = await ArticleModel.find().populate("author");
         res.json(data);
     }
     catch(error){
